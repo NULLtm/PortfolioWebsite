a87ed42de5dbf5221bdd318ce16fae26ac265468
NULLtm <31399167+NULLtm@users.noreply.github.com>
2022-12-20 17:31:42 -0800

    Working with an initial design. Thinking of changing this pretty soon. Possibly a more apple-esque design.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Menu from './Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu/>
     </div>
   );
 }
---
+++ src/Menu.js
@@ -0,0 +1,12 @@
+// Floating menu seen on home page
+import './Menu.css'
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <span className="menuText">NULLtheMOMENT</span>
+        </div>
+    );
+}
+
+export default Menu;
