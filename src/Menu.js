938d294e35783317ddd1bbc74c2047bd5afe499c
NULLtm <31399167+NULLtm@users.noreply.github.com>
2022-12-21 16:14:12 -0800

    Got the style I was going for down. Menu is almost done, then onto coding the individual pages

--- src/Menu.js
+++ src/Menu.js
@@ -2,10 +2,20 @@
 import './Menu.css'
 
 const Menu = () => {
+
+
+    const buttonClicked = (event) => {
+    }
+
     return (
         <div className="menu">
-            <span className="menuText">Owen Boseley</span>
-            <span className="typeBar menuText">_</span>
+            <div className="titleText menuItem">
+                <span>Owen Boseley</span>
+                <span className="typeBar">_</span>
+            </div>
+            <div onClick={buttonClicked} className="menuItem menuButton">About Me</div>
+            <div onClick={buttonClicked} className="menuItem menuButton">Projects</div>
+            <div onClick={buttonClicked} className="menuItem menuButton">Links</div>
         </div>
     );
 }
