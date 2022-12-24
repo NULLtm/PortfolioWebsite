9946c6f7652d8bd397f1162c56b7ccfe5de88795
NULLtm <31399167+NULLtm@users.noreply.github.com>
2022-12-24 10:14:24 -0800

    Started working on about me page. Looks pretty decent so far. It is based on the build your apple device screen.

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,23 @@
+import '../styles/AboutMe.css'
 
 const AboutMe = () => {
     return (
-        <div>ABOUT ME</div>
+        <div className="aboutContainer">
+            <div className="selectorContainer">
+                <div className="selectorText">
+                    <span className="selectorTitle1">Model.</span> <span className="selectorTitle2">Which is best for you?</span>
+                </div>
+                <div className="selector">
+                    <div className="selectorPrimaryLabel">Human</div>
+                    <div className="selectorPrimaryLabelSubtext">19 yr, 5'8"</div>
+                    <div className="selectorSecondaryLabels">
+                        <div>Owen Boseley</div>
+                        <div>Red Hair</div>
+                        <div>Green Eyes</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
