2c8629f900d4c6b681b11cd7c67ad647ed576b2b
NULLtm <31399167+NULLtm@users.noreply.github.com>
2022-12-28 22:38:57 -0800

    Projects Page v1
    Full implementation, with the exception of the lang colors which I am working on bug fixing right now,

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,25 +6,28 @@ import Project from "./SubComponents/Project";
 const Projects = () => {
 
     const [loaded, setLoaded] = useState(false);
+    const [data, setData] = useState(null);
     const URL_API = "https://api.github.com/users/NULLtm/repos";
-
     // API call to get projects to display
     if(!loaded) {
         axios.get(URL_API).then(res => {
             setLoaded(true);
-
-            // res is the data we got
-
+            setData(res.data);
         });
     }
 
     return (
-        <div className="page">
+        <div className="page projectsPage">
             {loaded ? (
-                <Project title="TestProject" status="Public"
-                desc="This is a test project."
-                stars={100}/>
-            ) : "Loading..."}
+                [...Array(data.length)].map((e, i) =>
+                  <Project title={data[i].name}
+                  status={data[i].private ? "Private" : "Public"}
+                           desc={data[i].description}
+                           lang={data[i].language}
+                           stars={data[i].stargazers_count}
+                           forks={data[i].forks_count}
+                  />
+                )) : "Loading..."}
         </div>
     );
 }
